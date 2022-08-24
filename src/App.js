9c1365883d31a15f6b1b64fb0eb1b1e66baccecd
Mainu <mainu.co.cc@gmail.com>
2022-08-24 16:25:59 +0600

    :sparkles: Setup husky

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const a = 20;
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
